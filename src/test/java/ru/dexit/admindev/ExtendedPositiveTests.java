aa2840b875656c928f228b18e7065f228c8ac3f0
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-03-28 17:10:53 +0200

    add expented positive tests - Employee - addEmployee (name, id)

---
+++ src/test/java/ru/dexit/admindev/ExtendedPositiveTests.java
@@ -0,0 +1,86 @@
+package ru.dexit.admindev;
+
+import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.dexit.admindev.assertions.AssertionsEmployee;
+import ru.dexit.admindev.helpers.CoreApiMethodsEmployee;
+import ru.dexit.admindev.models.Employee.AddEmployeeRequestModel;
+
+@DisplayName("Расширенные позитивные тесты")
+public class ExtendedPositiveTests extends TestBase{
+
+    @Epic("Employee")
+    @Story("Создание сотрудника")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Создание сотрудника c валидным именем:")
+    @Description("Тест создаёт сотрудника с валидными именами")
+    @ParameterizedTest
+    @MethodSource("ru.dexit.admindev.data.DataGenerator#getValidEmployeeNames")
+    public void testAddEmployeeWithValidNames(String name){
+
+        AddEmployeeRequestModel requestBody = AddEmployeeRequestModel.builder()
+                .name(name)
+                .roleId("b2a142fe-9035-46a5-bd9f-06baf40be2b0")
+                .email(faker.internet().emailAddress())
+                .build();
+
+        Response response = CoreApiMethodsEmployee.addEmployee(requestBody);
+        AssertionsEmployee.employeeAddedSuccessfully(response, requestBody);
+
+    }
+
+    @Epic("Employee")
+    @Story("Создание сотрудника")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Создание сотрудника c существуюшим именем")
+    @Description("Тест создаёт сотрудника с существуюшим именем")
+    @Test
+    public void testAddEmployeeWithExistName(){
+
+        String name = faker.name().fullName();
+
+        AddEmployeeRequestModel requestBodyFirstEmployee = AddEmployeeRequestModel.builder()
+                .name(name)
+                .roleId("b2a142fe-9035-46a5-bd9f-06baf40be2b0")
+                .email(faker.internet().emailAddress())
+                .build();
+
+        Response responseFirstEmployee = CoreApiMethodsEmployee.addEmployee(requestBodyFirstEmployee);
+
+        AddEmployeeRequestModel requestBodySecondEmployee = AddEmployeeRequestModel.builder()
+                .name(name)
+                .roleId("b2a142fe-9035-46a5-bd9f-06baf40be2b0")
+                .email(faker.internet().emailAddress())
+                .build();
+
+        Response responseSecondEmployee = CoreApiMethodsEmployee.addEmployee(requestBodySecondEmployee);
+
+        AssertionsEmployee.employeeAddedSuccessfully(responseSecondEmployee, requestBodySecondEmployee);
+
+    }
+
+    @Epic("Employee")
+    @Story("Создание сотрудника")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Создание сотрудника c валидной ролью")
+    @Description("Тест создаёт сотрудника с валидными ролями")
+    @ParameterizedTest
+    @MethodSource("ru.dexit.admindev.data.DataGenerator#getValidRoles")
+    public void testAddEmployeeWithValidRoles(String role){
+
+        AddEmployeeRequestModel requestBody = AddEmployeeRequestModel.builder()
+                .name(faker.name().fullName())
+                .roleId(role)
+                .email(faker.internet().emailAddress())
+                .build();
+
+        Response response = CoreApiMethodsEmployee.addEmployee(requestBody);
+        AssertionsEmployee.employeeAddedSuccessfully(response, requestBody);
+
+    }
+
+}
--- src/test/java/ru/dexit/admindev/data/DataGenerator.java
+++ src/test/java/ru/dexit/admindev/data/DataGenerator.java
@@ -1,15 +1,26 @@
 package ru.dexit.admindev.data;
 
 import com.github.javafaker.Faker;
+import org.junit.jupiter.params.provider.Arguments;
 import ru.dexit.admindev.models.Employee.AddEmployeeRequestModel;
 
 import ru.dexit.admindev.models.Role.AddRoleRequestModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class DataGenerator {
     static Faker faker = new Faker();
+    static final String engLettersLowerCase = "abcdefghijklmnopqrstuvwxyz";
+    static final String engLettersUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static final String engLetters = engLettersLowerCase + engLettersUpperCase;
+    static final String rusLettersLowerCase = "абвгдежзийклмнопрстуфхцчшщыьэюя";
+    static final String rusLettersUpperCase = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ";
+    static final String rusLetters = rusLettersLowerCase + rusLettersUpperCase;
 
     public static AddEmployeeRequestModel getRandomAddEmployeeRequestModel(){
 
@@ -32,8 +43,64 @@ public class DataGenerator {
         policies.add("user.read");
 
         return AddRoleRequestModel.builder()
-                .name(faker.company().profession())
+                .name(faker.company().profession() + "_" + generateRandomString(engLetters, 6))
                 .policies(policies)
                 .build();
+    }
+
+    public static Stream<Arguments> getValidEmployeeNames(){
+
+        return Stream.of(
+                arguments(generateRandomString(rusLettersLowerCase, 3)),
+                arguments(generateRandomString(rusLettersLowerCase, 25)),
+                arguments(generateRandomString(rusLettersLowerCase, 50)),
+                arguments(generateRandomString(rusLettersUpperCase, 3)),
+                arguments(generateRandomString(rusLettersUpperCase, 3) + "."),
+                arguments(generateRandomString(rusLettersUpperCase, 25)),
+                arguments(generateRandomString(rusLettersUpperCase, 50)),
+                arguments(generateRandomString(rusLetters, 24) + "." + generateRandomString(rusLetters, 24)),
+                arguments(generateRandomString(engLettersLowerCase, 3)),
+                arguments(generateRandomString(engLettersLowerCase, 25)),
+                arguments(generateRandomString(engLettersLowerCase, 50)),
+                arguments(generateRandomString(engLettersUpperCase, 3)),
+                arguments(generateRandomString(engLettersUpperCase, 3) + "."),
+                arguments(generateRandomString(engLettersUpperCase, 25)),
+                arguments(generateRandomString(rusLetters, 12) + generateRandomString(engLetters, 12)),
+                arguments(generateRandomString(rusLetters, 12) + " " + generateRandomString(engLetters, 12)),
+                arguments(generateRandomString(rusLetters, 5) + " " + generateRandomString(rusLetters, 12) + " " + generateRandomString(rusLetters, 12)),
+                arguments(generateRandomString(rusLetters, 5) + "'" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + "[" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + "]" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + "^" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + "_" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + "`" + " " + generateRandomString(rusLetters, 5)),
+                arguments(generateRandomString(rusLetters, 5) + " " + " " + generateRandomString(rusLetters, 5))
+        );
+    }
+
+    public static Stream<Arguments> getValidRoles(){
+        return Stream.of(
+                arguments(UserRole.FULL_WRITE.role.uid),
+                arguments(UserRole.FULL_READ.role.uid),
+                arguments(UserRole.NO_RIGHTS.role.uid)
+        );
+    }
+
+
+    public static Integer generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(1000);
+    }
+
+    public static String generateRandomString(String characters, int length) {
+        StringBuilder randomString = new StringBuilder();
+
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            randomString.append(characters.charAt(randomIndex));
+        }
+
+        return randomString.toString();
     }
 }
