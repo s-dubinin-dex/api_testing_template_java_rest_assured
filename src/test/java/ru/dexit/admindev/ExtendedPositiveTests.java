e0bd7095d4bb1eaa5014f3f1629905da3bb15a83
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-03-29 17:17:23 +0200

    add Employee / updateEmployee tests

--- src/test/java/ru/dexit/admindev/ExtendedPositiveTests.java
+++ src/test/java/ru/dexit/admindev/ExtendedPositiveTests.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.dexit.admindev.assertions.AssertionsEmployee;
+import ru.dexit.admindev.data.DataGenerator;
 import ru.dexit.admindev.data.Role;
 import ru.dexit.admindev.helpers.CoreApiMethodsEmployee;
 import ru.dexit.admindev.models.Employee.AddEmployeeRequestModel;
+import ru.dexit.admindev.models.Employee.EmployeeCommonResponseModel;
+import ru.dexit.admindev.models.Employee.UpdateEmployeeRequestModel;
 
 @DisplayName("Расширенные позитивные тесты")
 public class ExtendedPositiveTests extends TestBase{
@@ -100,6 +103,77 @@ public class ExtendedPositiveTests extends TestBase{
 
             Response response = CoreApiMethodsEmployee.addEmployee(requestBody);
             AssertionsEmployee.employeeAddedSuccessfully(response, requestBody);
+    }
+
+    @Feature("Employee")
+    @Story("Изменение сотрудника")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение сотрудника c валидным ID")
+    @Description("Тест изменяет сотрудника c валидным ID")
+    @Test
+    public void testUpdateEmployeeWithValidId(){
+
+        AddEmployeeRequestModel requestBodyCreation = DataGenerator.getRandomAddEmployeeRequestModel();
+        Response responseCreation = CoreApiMethodsEmployee.addEmployee(requestBodyCreation);
+        EmployeeCommonResponseModel responseBodyCreation = responseCreation.as(EmployeeCommonResponseModel.class);
+
+        UpdateEmployeeRequestModel requestBody = UpdateEmployeeRequestModel.builder()
+                .id(responseBodyCreation.id)
+                .name(faker.name().fullName())
+                .roleId(Role.FULL_READ.roleUUID)
+                .build();
+
+        Response response = CoreApiMethodsEmployee.updateEmployee(requestBody);
+        AssertionsEmployee.employeeUpdatedSuccessfully(response, requestBody, responseBodyCreation);
+
+    }
+
+    @Feature("Employee")
+    @Story("Изменение сотрудника")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение сотрудника c валидным именем")
+    @Description("Тест изменяет сотрудника c валидным именем")
+    @ParameterizedTest
+    @MethodSource("ru.dexit.admindev.data.DataGenerator#getValidEmployeeNames")
+    public void testUpdateEmployeeWithValidName(String name){
+
+        AddEmployeeRequestModel requestBodyCreation = DataGenerator.getRandomAddEmployeeRequestModel();
+        Response responseCreation = CoreApiMethodsEmployee.addEmployee(requestBodyCreation);
+        EmployeeCommonResponseModel responseBodyCreation = responseCreation.as(EmployeeCommonResponseModel.class);
+
+        UpdateEmployeeRequestModel requestBody = UpdateEmployeeRequestModel.builder()
+                .id(responseBodyCreation.id)
+                .name(name)
+                .roleId(Role.FULL_READ.roleUUID)
+                .build();
+
+        Response response = CoreApiMethodsEmployee.updateEmployee(requestBody);
+        AssertionsEmployee.employeeUpdatedSuccessfully(response, requestBody, responseBodyCreation);
+
+    }
+
+    @Feature("Employee")
+    @Story("Изменение сотрудника")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение сотрудника валидными ролями")
+    @Description("Тест изменяет сотрудника валидными ролями")
+    @ParameterizedTest
+    @MethodSource("ru.dexit.admindev.data.DataGenerator#getValidRoles")
+    public void testUpdateEmployee(String role){
+
+        AddEmployeeRequestModel requestBodyCreation = DataGenerator.getRandomAddEmployeeRequestModel();
+        Response responseCreation = CoreApiMethodsEmployee.addEmployee(requestBodyCreation);
+        EmployeeCommonResponseModel responseBodyCreation = responseCreation.as(EmployeeCommonResponseModel.class);
+
+        UpdateEmployeeRequestModel requestBody = UpdateEmployeeRequestModel.builder()
+                .id(responseBodyCreation.id)
+                .name(faker.name().fullName())
+                .roleId(role)
+                .build();
+
+        Response response = CoreApiMethodsEmployee.updateEmployee(requestBody);
+        AssertionsEmployee.employeeUpdatedSuccessfully(response, requestBody, responseBodyCreation);
+
     }
 
 }
